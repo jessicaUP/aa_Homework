092444790454c9e4410603dc2ae434675c6c1de4
jessicaUP <uphoff.jessica@gmail.com>
2021-03-15 21:06:43 -0400

    Create initialize

---
+++ W4D1/graph_nodes.rb
@@ -0,0 +1,20 @@
+class GraphNode
+    attr_accessor :neighbors
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @neighbors = []
+    end
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
