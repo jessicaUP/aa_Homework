0e0b0155c3cf39920dadb0c6d7559ad5cd2ceda9
jessicaUP <uphoff.jessica@gmail.com>
2021-03-16 20:01:48 -0400

    Add new files

--- W4D1/graph_nodes.rb
+++ W4D1/graph_nodes.rb
@@ -6,15 +6,8 @@ class GraphNode
         @parent = nil
         @neighbors = []
     end
-end
 
-a = GraphNode.new('a')
-b = GraphNode.new('b')
-c = GraphNode.new('c')
-d = GraphNode.new('d')
-e = GraphNode.new('e')
-f = GraphNode.new('f')
-a.neighbors = [b, c, e]
-c.neighbors = [b, d]
-e.neighbors = [a]
-f.neighbors = [e]
+    def bfs(starting_node, target_value)
+
+    end
+end
