18021e6e24df92bb6f972d98febbb0a113103a79
jessicaUP <uphoff.jessica@gmail.com>
2021-03-21 23:52:19 -0400

    Define add, count and show methods

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -2,21 +2,33 @@
 
     def initialize(limit)
         @limit = limit
-        @hashmap = Hash.new(limit)
-        @link_list = 
+        @cache = []
     end
 
     def count
       # returns number of elements currently in cache
-      @hashmap.count
+      @cache.length
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if @cache.include?(el)
+        @cache.delete(el)
+        @cache << el
+      end
+      
+      if @cache.length < @limit
+        @cache << el
+      else 
+        @cache.shift
+        @cache << el
+      end
+
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      @cache.reverse
     end
 
     private
@@ -29,22 +41,22 @@
   
   
   
-  johnny_cache = LRUCache.new(4)
+   johnny_cache = LRUCache.new(4)
 
-  johnny_cache.add("I walk the line")
-  johnny_cache.add(5)
+   johnny_cache.add("I walk the line")
+   johnny_cache.add(5)
 
-  johnny_cache.count # => returns 2
+  p johnny_cache.count # => returns 2
 
-  johnny_cache.add([1,2,3])
-  johnny_cache.add(5)
-  johnny_cache.add(-5)
-  johnny_cache.add({a: 1, b: 2, c: 3})
-  johnny_cache.add([1,2,3,4])
-  johnny_cache.add("I walk the line")
-  johnny_cache.add(:ring_of_fire)
-  johnny_cache.add("I walk the line")
-  johnny_cache.add({a: 1, b: 2, c: 3})
+  p johnny_cache.add([1,2,3])
+  p johnny_cache.add(5)
+  p johnny_cache.add(-5)
+  p johnny_cache.add({a: 1, b: 2, c: 3})
+  p johnny_cache.add([1,2,3,4])
+  p johnny_cache.add("I walk the line")
+  p johnny_cache.add(:ring_of_fire)
+  p johnny_cache.add("I walk the line")
+  p johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
