b14b4ac804eab513efe8f76b569aaa0e87781f04
jessicaUP <uphoff.jessica@gmail.com>
2021-05-03 08:57:13 -0400

    Stage 1 & 2

--- W11D1/frontend/todo_redux.jsx
+++ W11D1/frontend/todo_redux.jsx
@@ -8,7 +8,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  
+  // store.dispatch = addLoggingToDispatch(store);
+
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// const addLoggingToDispatch = store => {
+//   const FirstDispatch = store.dispatch; // function copy
+//   return action => {
+//     console.log(store.getState()); // old state
+//     console.log(action);
+//     FirstDispatch(action); // call action
+//     console.log(store.getState()); // new state
+//   }
+// }
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+}
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch
+  middlewares.forEach( middleware => {
+    dispatch = middleware(store)(dispatch);
+  })
+  return Object.assign ({}, store, {dispatch})
+}
